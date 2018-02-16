95a505f8343937efc9212a9da0b47545ad5d381d
Tânia Salgado de Abreu <taniasgdo@hotmail.com>
2018-02-16 00:46:53 -0200

    3.20 - Ex.2 Criando componente

--- exercicios_react/ex/index.jsx
+++ exercicios_react/ex/index.jsx
@@ -1,4 +1,4 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
-ReactDOM.render(<h1>Olá React</h1>, document.getElementById('app'))
+import Component from './component'
+ReactDOM.render(<Component/>, document.getElementById('app'))
