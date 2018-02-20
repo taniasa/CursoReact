936feb5379388f3ed5b6439f4ebb0b136a683f5d
Tânia Salgado de Abreu <taniasgdo@hotmail.com>
2018-02-19 23:57:21 -0300

    Aula 4.46 - Criando rotas erro digitação

--- todo-app/frontend/src/main/routes.jsx
+++ todo-app/frontend/src/main/routes.jsx
@@ -5,9 +5,9 @@ import Todo from '../todo/todo'
 import About from '../about/about'
 
 export default props => (
-    <Router history={hashHistory}>
-        <Route path='/todos' component={Todo} />
-        <Route path='/about' component={About} />
+    <Router history={hasHistory}>
+        <Route path='/todos' component={Todo}/>
+        <Route path='/about' component={About}/>
         <Redirect from='*' to='/todos' />
     </Router>
 )
