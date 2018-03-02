1e88c8052c02ba6fb61afb771b75bd972a849cbc
Tânia Salgado de Abreu <taniasgdo@hotmail.com>
2018-03-02 00:31:13 -0300

    Aula 7.75 - Configurando Redux DevTools

--- todo-app/frontend/src/index.jsx
+++ todo-app/frontend/src/index.jsx
@@ -9,7 +9,9 @@ import reducers from './main/reducers'
 // import { combineReducers, createStore } from 'redux'
 // import { Provider } from 'react-redux'
 
-const store = createStore(reducers)
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ 
+    && window.__REDUX_DEVTOOLS_EXTENSION__()
+const store = createStore(reducers, devTools)
 ReactDOM.render(
     <Provider store={store}>
         <App/>
--- todo-app/frontend/src/todo/todoActions.js
+++ todo-app/frontend/src/todo/todoActions.js
@@ -1,4 +1,4 @@
 export const changeDescription = event => ({
     type: 'DESCRIPTION_CHANGED',
-    payload: event.targe.value
+    payload: event.target.value
 })
