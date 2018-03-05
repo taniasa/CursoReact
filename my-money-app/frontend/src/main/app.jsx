d60ac1872227b428bd6d4179a286f1fe169a4569
Tânia Salgado de Abreu <taniasgdo@hotmail.com>
2018-03-05 01:16:01 -0300

    Aula 9.108 -  Componente Rodapé

--- my-money-app/frontend/src/main/app.jsx
+++ my-money-app/frontend/src/main/app.jsx
@@ -3,9 +3,15 @@ import '../common/template/dependencies'
 
 import Header from '../common/template/header'
 import SideBar from  '../common/template/sidebar'
+import Foorter from '../common/template/footer'
+
 export default props => (
     <div className='wrapper'>
         <Header/>
         <SideBar/>
+        <div className='content-wrapper'>
+            <h1>Conteúdo</h1>
+        </div>
+        <Foorter/>
     </div>
 )
