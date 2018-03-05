ba53802bee5547363df6f18fdc80dfa0445d0a19
Tânia Salgado de Abreu <taniasgdo@hotmail.com>
2018-03-05 01:00:45 -0300

    Aula 9.10 -  Componente menu parte 1

--- my-money-app/frontend/src/main/app.js
+++ my-money-app/frontend/src/main/app.js
@@ -2,9 +2,10 @@ import React from 'react'
 import '../common/template/dependencies'
 
 import Header from '../common/template/header'
-
+import SideBar from  '../common/template/sidebar'
 export default props => (
     <div className='wrapper'>
         <Header/>
+        <SideBar/>
     </div>
 )
