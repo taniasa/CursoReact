44731cdbf7a6aadbbe80361cc745ac7c3d2408fa
Tânia Salgado de Abreu <taniasgdo@hotmail.com>
2018-03-08 22:59:24 -0300

    Aula 9.144 - Alterar ciclo de pagamento

--- my-money-app/frontend/src/billingCycle/billingCycle.jsx
+++ my-money-app/frontend/src/billingCycle/billingCycle.jsx
@@ -12,7 +12,7 @@ import TabContent from '../common/tab/tabContent'
 import { selectTab, showTabs } from '../common/tab/tabActions'
 import List from './billingCycleList'
 import Form from './billingCycleForm'
-import {create } from './biilingCycleActions'
+import {create, update } from './biilingCycleActions'
 class BillingCycle extends Component {
     componentWillMount() {
         this.props.selectTab('tabList')
@@ -39,7 +39,7 @@ class BillingCycle extends Component {
                                 <Form onSubmit={this.props.create} />
                             </TabContent>
                             <TabContent id='tabUpdate'>
-                                <Form />
+                                <Form onSubmit={this.props.update}/>
                             </TabContent>
                             <TabContent id='tabDelete'>
                                 <h1>Excluir</h1>
@@ -52,5 +52,5 @@ class BillingCycle extends Component {
     }
 }
 
-const mapDispatchToProps = dispatch => bindActionCreators({selectTab, showTabs, create}, dispatch)
+const mapDispatchToProps = dispatch => bindActionCreators({selectTab, showTabs, create, update}, dispatch)
 export default connect(null, mapDispatchToProps)(BillingCycle)
