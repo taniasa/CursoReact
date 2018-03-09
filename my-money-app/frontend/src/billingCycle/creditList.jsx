479972c242197b10e60fe8fb3550c74720916092
Tânia Salgado de Abreu <taniasgdo@hotmail.com>
2018-03-09 00:29:38 -0300

    Aula 9.150 -  Adicionar e Clonar Créditos

--- my-money-app/frontend/src/billingCycle/creditList.jsx
+++ my-money-app/frontend/src/billingCycle/creditList.jsx
@@ -7,6 +7,18 @@ import Input from '../common/form/input'
 // import If from '../common/operator/if'
 
 class CreditList extends Component {
+    add(index, item = {}) {
+        if(!this.props.readOnly) {
+            this.props.arrayInsert('billingCycleForm', 'credits', index, item)
+        }
+    }
+
+    remove(index) {
+        if(!this.props.readOnly && this.props.list.length > 1) {
+            this.props.arrayRemove('billingCycleForm', this.props.field, index)
+        }
+    }
+
     renderRows() {
         const list = this.props.list || []
         return list.map((item, index) => (
@@ -16,7 +28,20 @@ class CreditList extends Component {
                     /></td>
                 <td><Field name={`credits[${index}].value`} component={Input}
                     placeholder='Informe o valor' readOnly={this.props.readOnly}/></td>
-                <td></td>
+                 <td>
+                    <button type='button' className='btn btn-success'
+                        onClick={() => this.add(index + 1)}>
+                        <i className="fa fa-plus"></i>
+                    </button>
+                    <button type='button' className='btn btn-warning'
+                        onClick={() => this.add(index + 1, item)}>
+                        <i className="fa fa-clone"></i>
+                    </button>
+                    <button type='button' className='btn btn-danger'
+                        onClick={() => this.remove(index)}>
+                        <i className="fa fa-trash-o"></i>
+                    </button>
+                </td>
             </tr>
         ))
     }
@@ -43,4 +68,5 @@ class CreditList extends Component {
         )
     }
 }
-export default CreditList
+const mapDispatchToProps = dispatch => bindActionCreators({ arrayInsert, arrayRemove }, dispatch)
+export default connect(null, mapDispatchToProps)(CreditList)
